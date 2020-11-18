538215895bab652f63c6781ec303eaee69d30b17
Krisztian Der <krisztian.der@gmail.com>
2020-11-18 21:21:35 +0000

    Day 2 simplify id validation and tests

--- Day 2/modules/exercise_validation.py
+++ Day 2/modules/exercise_validation.py
@@ -6,27 +6,25 @@ def cleanup(**kwargs):
         print('Choose one of the following categories: users, posts, albums, photos. Defaulting to photos')
         category = 'photos'
 
-    id = 1
-    if 'id' in kwargs:
-            id = int(float(kwargs['id']))
-            if id not in range(1,9):
-                print('Provide an id between 1-8. Defaulting to 1.')
-                id = 1
+    id = int(float(kwargs['id']))
+    if not id or id not in range(1,9):
+        print('Provide an id between 1-8. Defaulting to 1.')
+        id = 1
     return {'category': category, 'id': id}
     
 
 if __name__ == '__main__':
     result = cleanup(category='users', id=3)
-    print(result)
+    print('result1: ', result)
     result2 = cleanup(category='something', id=1)
-    print(result2)
+    print('result2: ', result)
     result3 = cleanup(category='', id=1)
-    print(result3)
+    print('result3: ', result)
     result4 = cleanup(category='PHOTOS', id=1)
-    print(result4)
+    print('result4: ', result)
     result5 = cleanup(category='photos', id=0)
-    print(result5)
+    print('result5: ', result)
     result6 = cleanup(category='users', id=3.2)
-    print(result6)
+    print('result6: ', result)
     result7 =cleanup(category='users', id='3.4252')
 
